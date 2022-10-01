ad05ee63bff72dd2dcf14c9642d940638d0f51f1
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-10-01 21:50:48 +0530

    add places item comp

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,12 +1,15 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import PlaceItem from "./PlaceItem";
 
 const PlacesList = ({ places }) => {
   return (
     <FlatList
       data={places}
       keyExtractor={(place) => place.id}
-      renderItem={() => {}}
+      renderItem={({ item }) => {
+        <PlaceItem place={item} />;
+      }}
     >
       <Text>PlacesList</Text>
     </FlatList>
