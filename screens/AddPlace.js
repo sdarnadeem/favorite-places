802e4e8f6df09a31585ce4cd8fd0f6a0a08dc743
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-10-01 20:39:32 +0530

    add all screens

---
+++ screens/AddPlace.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AddPlace = () => {
+  return (
+    <View>
+      <Text>AddPlace</Text>
+    </View>
+  );
+};
+
+export default AddPlace;
+
+const styles = StyleSheet.create({});
