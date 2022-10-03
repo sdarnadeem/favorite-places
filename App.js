3d44493a0ef7faaca6aad6e4f5476e0b699c1e32
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-10-03 15:15:44 +0530

    add sql database

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import AppLoading from "expo-app-loading";
+import { Text, View } from "react-native";
+import Colors from "./constants/colors";
 import Navigation from "./Navigation";
 import { init } from "./utils/database";
 
 export default function App() {
   const [dbInitialized, setDbInitialized] = useState(false);
   useEffect(() => {
-    init()
-      .then(() => {
+    async function db() {
+      try {
+        await init();
         setDbInitialized(true);
-      })
-      .catch();
+      } catch (e) {}
+    }
+    db();
   }, []);
 
   if (!dbInitialized) {
-    return <AppLoading />;
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text
+          style={{ fontWeight: "bold", fontSize: 20, color: Colors.accent500 }}
+        >
+          Loading...
+        </Text>
+      </View>
+    );
   }
 
   return (
